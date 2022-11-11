459bb12f256997f6a44ae45999969f0054f9c327
Santi405 <zaninsantino@gmail.com>
2022-11-11 02:31:37 -0300

    TableroTestFail

---
+++ tests/Feature/TableroTestFail.php
@@ -0,0 +1,45 @@
+<?php
+namespace Tests\Feature;
+namespace App;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class TableroTest extends TestCase{
+/** @test */
+public function TestearTablero(){
+
+// Creo un tablero de (x,y) dimensiones y dos variables con los colores de la ficha
+$tablero = new Tablero(5,5);
+$FichaRoja = new Ficha('rojo');
+$FichaAzul = new Ficha('azul');
+
+// Suelto fichas en posiciones al azar
+$tablero->TirarFicha($FichaRoja, 3);
+$tablero->TirarFicha($FichaAzul, 3);
+$tablero->TirarFicha($FichaRoja, 3);
+$tablero->TirarFicha($FichaAzul, 8);
+$tablero->TirarFicha($FichaRoja, 3);
+$tablero->TirarFicha($FichaAzul, 4);
+$tablero->TirarFicha($FichaRoja, 3);
+$tablero->TirarFicha($FichaAzul, 4);
+
+$redAmount = 0;
+$blueAmount = 0;
+$whiteAmount = 0;
+
+// Cuento fichas rojas y azules en el tablero
+for ($x = 0; $x < $tablero->getHeight(); $x++) {
+    for ($y = 0; $y < $tablero->getWidth(); $y++) {
+    if($tablero->cuadricul[$x][$y]->fichaColor() == 'azul'){
+    $blueAmount++;
+    }else if(($tablero->cuadricul[$x][$y]->fichaColor()) == 'rojo'){
+            $redAmount++;
+    }else $whiteAmount++;
+    }
+}
+
+$this->assertTrue(!($whiteAmount == (25-($redAmount+$blueAmount))));
+
+}
+}
