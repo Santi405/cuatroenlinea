6bb38ae5380880bcb625d47b9a2a41761925632f
Santi405 <zaninsantino@gmail.com>
2022-11-10 22:40:24 -0300

    Clase Ficha

--- app/ficha.php
+++ app/ficha.php
@@ -1,14 +1,20 @@
 <?php
 namespace App;
 
+use Exception;
+
 interface FichaInter{
     public function fichaColor();
 }
 
 class Ficha implements FichaInter{
-    protected int $color;
-    function __construct(int $color){
-        $this->$color = $color
+    protected string $color;
+
+    public function __construct(string $color){
+        if (!($color == 'blanco' || $color == 'rojo' || $color == 'azul')) {
+			throw new \Exception("El color de la ficha es erroneo\n");
+			return;
+		}
     }
 
     public function fichaColor(){
